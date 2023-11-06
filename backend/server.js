c98351aa07e73a979f13d19f12e20f6dacad6a3e
Eloi <eloi.random@gmail.com>
2023-11-06 19:09:33 +0100

    v2 Modification de server.js

--- backend/server.js
+++ backend/server.js
@@ -70,17 +70,46 @@
 // //Configure le port que doit écouter le serveur - selon la valeur qui à étais défini plutôt
 // server.listen(port);
 
-const http = require('http');
+const http = require("http");
+// //Imporation du package app
+const app = require("./app");
+require("dotenv").config();
 
-const ip = 'fd00::5:c63';
-const port = 8100;
+// const ip = "fd00::5:c63";
+// const port = 8100;
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Bienvenue sur votre serveur HTTP !');
-});
+const server = http.createServer(app);
+const errorHandler = (error) => {
+    //Si l'erreur n'est pas strictement égale à listen alors elle est jeter
+    if (error.syscall !== "listen") {
+        throw error;
+    }
+    //Permet de récupérer l'adresse du serveur
+    const address = server.address();
+    //Variable crée en fonction du type d'adresse | Si address est chaine de caractère elle est préfixer par pipe, sinon elle est préfixer par port suivit du numéro de port
+    const bind = typeof address === "string" ? "pipe " + address : "port: " + port;
 
-server.listen(port, ip, () => {
-  console.log(`Le serveur écoute à l'adresse IP: ${ip}, sur le port: ${port}`);
+    switch (error.code) {
+        //Si jamais le code d'erreur est 'EACCES' alors le msg d'erreur dit que ont a pas de privilège assez élever
+        case "EACCES":
+            console.error(bind + " requires elevated privileges.");
+            //Arrête l'exécution de l'application Node.js avec '1' comme argument | '1' est un code de sortie qui signifie que cela c'est mal terminer
+            process.exit(1);
+            //Arrêt du proccessus
+            break;
+        //Si le code d'erreur est 'EADDRINUSE', cela signifie que le port est déjà utilisé
+        case "EADDRINUSE":
+            console.error(bind + " is already in use.");
+            process.exit(1);
+            //Arrêt du proccessus
+            break;
+        //Par défault
+        default:
+            //Signale l'erreur et termine l'exécution
+            throw error;
+    }
+};
+server.on("error", errorHandler);
+server.on("listening", () => {
+    console.log("Serveur lancer");
 });
